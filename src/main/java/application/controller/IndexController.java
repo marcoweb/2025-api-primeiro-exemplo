4c63d20ce4df52afd867e1180ba6bbba1ee41ed4
Marco Antônio Gusmão Carvalho <marcocarvalho.web@gmail.com>
2025-02-18 19:42:58 +0000

    Aula 18/02/2025

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -2,17 +2,30 @@ package application.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+//import java.util.Arrays;
+import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class IndexController {
-    @GetMapping("/")
-    public String home() {
-        return "home - GET";
+    //private List<String> frutas = Arrays.asList("Maça", "Banana", "Morango");
+    private List<String> frutas = new ArrayList<String>();
+
+    @GetMapping
+    public List<String> home() {
+        return frutas;
+        //return "home - GET";
     }
 
-    @PostMapping("/")
-    public String homePost() {
-        return "home - POST";
+    @PostMapping
+    public List<String> homePost(@RequestBody String fruta) {
+        frutas.add(fruta);
+        return frutas;
+        // return "home - POST";
     }
 }
